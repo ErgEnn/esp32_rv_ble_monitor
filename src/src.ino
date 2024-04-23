a130811188bd3f35beb9d6583a10d99b1a916948
Ergo Enn <ergo97@gmail.com>
2024-04-23 21:32:56 +0300

    Add timeout logic

--- src/src.ino
+++ src/src.ino
@@ -26,29 +26,30 @@ TFT_eSPI tft = TFT_eSPI();
 #define TAG_RUUVI   1
 #define TAG_MOPEKA  2
 #define TAG_DUAL_FLOAT  3
+#define TAG_SINGLE_FLOAT  4
 
 BLEScan *pBLEScan;
 
 typedef struct KeyValuePair {
   const char* address;
   int tagType;
-  float value;
-  float previousValue;
+  char value[8];
+  char previousValue[8];
   const char* text;
-  const char* unit;
+  unsigned long lastUpdate;
 };
 
 #define TAG_COUNT 9
 KeyValuePair addresses[TAG_COUNT] = {
-  {"", TAG_RUUVI, 0.0f, 0.1f, "Vesi", "L"},
-  {"48:70:1e:92:20:f2", TAG_MOPEKA, 0.0f, 0.1f, "Gaas 1", "%"},
-  {"", TAG_MOPEKA, 0.0f, 0.1f, "Gaas 2", "%"},
-  {"0c:b8:15:cd:31:fe", TAG_DUAL_FLOAT, 0.0f, 0.1f, "Must", "%"},
-  {"alt", TAG_DUAL_FLOAT, 0.0f, 0.1f, "Hall", "%"},
-  {"", TAG_RUUVI, 0.0f, 0.1f, "Kulmik", "`C"},
-  {"", TAG_RUUVI, 0.0f, 0.1f, "Sugavkulm", "`C"},
-  {"", TAG_RUUVI, 0.0f, 0.1f, "Valistemp", "`C"},
-  {"d4:aa:40:07:a8:0a", TAG_RUUVI, 0.0f, 0.1f, "Sisetemp", "`C"},
+  {"c8:c9:a3:c5:76:9e", TAG_SINGLE_FLOAT, "????", "", "Vesi", 0},
+  {"48:70:1e:92:20:f2", TAG_MOPEKA,       "????", "", "Gaas 1", 0},
+  {"00:00:00:00:00:00", TAG_MOPEKA,       "????", "", "Gaas 2", 0},
+  {"0c:b8:15:cd:31:fe", TAG_DUAL_FLOAT,   "????", "", "Must", 0},
+  {"ff:ff:ff:ff:ff:ff", TAG_DUAL_FLOAT,   "????", "", "Hall", 0},
+  {"00:00:00:00:00:00", TAG_RUUVI,        "????", "", "Kulmik", 0},
+  {"00:00:00:00:00:00", TAG_RUUVI,        "????", "", "Sugavkulm", 0},
+  {"00:00:00:00:00:00", TAG_RUUVI,        "????", "", "Valistemp", 0},
+  {"d4:aa:40:07:a8:0a", TAG_RUUVI,        "????", "", "Sisetemp", 0},
 };
 
 KeyValuePair* getTag(const char* address) {
@@ -79,14 +80,17 @@ void loop() {
   for (int i = 0; i < TAG_COUNT; i++)
   {
     KeyValuePair* tag = &addresses[i];
-    if(tag->value != tag->previousValue){
+    if(millis() - tag->lastUpdate > 10000){
+      //If no update in last 10 sec, change text
+      sprintf(tag->previousValue, "%s", tag->value);
+      strncpy(tag->value, "----  ", sizeof(tag->value));
+    }
+    if(strcmp(tag->value, tag->previousValue) != 0){
       tft.fillRect(0, drawy - 48/2, TFTW, 48, TFT_BLACK);
       tft.setTextDatum(3);
       tft.drawString(tag->text, 16, drawy);
-      char signedValue[6];
-      sprintf(signedValue, "%.1f %s", tag->value, tag->unit);
       tft.setTextDatum(5);
-      tft.drawString(signedValue, TFTW-16, drawy);
+      tft.drawString(tag->value, TFTW-16, drawy);
     }
     drawy = drawy + 48 + 2;
   }
@@ -108,6 +112,9 @@ class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
       case TAG_DUAL_FLOAT:
         handleDualFloat(device, tag);
         break;
+      case TAG_SINGLE_FLOAT:
+        handleSingleFloat(device, tag);
+        break;
       default:
         break;
     }
@@ -122,8 +129,8 @@ class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
   
     float temperature = raw_temperature * 0.005; // Celcius
     //float humidity = raw_humidity * 0.0025; // Percent
-    tag->previousValue = tag->value;
-    tag->value = temperature;
+
+    updateValue(tag, temperature, "%.1f `C");
   }
 
   void handleMopeka(BLEAdvertisedDevice device, KeyValuePair* tag){
@@ -136,17 +143,31 @@ class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
     for (uint8_t i = 8; i < 27; i++) {
       level ^= data[i];
     }
-    tag->previousValue = tag->value;
-    tag->value = level * .762; // cm
+    level = level * .762; // cm
+
+    updateValue(tag, level, "%.1f %%");
   }
 
   void handleDualFloat(BLEAdvertisedDevice device, KeyValuePair* tag){
     uint8_t* data = device.getPayload();
-    tag->previousValue = tag->value;
-    tag->value = data[10];
-    KeyValuePair* tag2 = getTag("alt");
-    tag2->previousValue = tag2->value;
-    tag2->value = data[9];
+
+    updateValue(tag, data[10], "%.0f %%");
+
+    KeyValuePair* tag2 = getTag("ff:ff:ff:ff:ff:ff");
+    updateValue(tag2, data[9], "%.0f %%");
+  }
+
+  void handleSingleFloat(BLEAdvertisedDevice device, KeyValuePair* tag){
+    uint8_t* data = device.getPayload();
+    updateValue(tag, data[9], "%.0f L");
+  }
+
+  void updateValue(KeyValuePair* tag, float value, char* format){
+    char newValue[8];
+    sprintf(newValue, format, value);
+    strncpy(tag->previousValue, tag->value, sizeof(tag->previousValue));
+    strncpy(tag->value, newValue, sizeof(tag->value));
+    tag->lastUpdate = millis();
   }
 };
 
